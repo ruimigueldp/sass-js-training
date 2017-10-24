fcc3d52493f056e9003572720ff00f4b59de6e41
Rui <rui@ruiparedes.local>
2017-10-24 18:49:29 +0100

    1st upload

--- src/js/link.js
+++ src/js/link.js
@@ -4,8 +4,16 @@ export class LinkHandler {
   }
 
   build () {
-    let link = 'https://www.youtube.com/embed/' + this.code;
-    let iframe = `<iframe src="${link}" frameborder="0" allowfullscreen></iframe>`;
-    document.querySelector('#play').innerHTML = iframe;
+    if (this.code.length === 11) {
+      // add class
+      document.getElementById('screen').classList.add('screen');
+      const link = 'https://www.youtube.com/embed/' + this.code;
+      const iframe = `<iframe src="${link}" frameborder="0" allowfullscreen></iframe>`;
+      document.querySelector('#play').innerHTML = iframe;
+    } else {
+      const imgSrc = './img/meme.png';
+      const errorImg = `<h4><span class="alert">! Invalid video id !<span></h4> <img src="${imgSrc}" alt="meme" width="30%" height="30%">`;
+      document.querySelector('#play').innerHTML = errorImg;
+    };
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,30 @@
 import { MoviesCollection } from './likes';
 import { LinkHandler } from './link';
 
+const hero = document.querySelector('.content-title');
+const text = hero.querySelector('h1');
+const walk = 25; // 100px
+// shadow function
+function shadow (e) {
+  const { offsetWidth: width, offsetHeight: height } = hero;
+  let { offsetX: x, offsetY: y } = e;
+
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  };
+
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
+  // .textShadow
+  text.style.textShadow = `${xWalk}px ${yWalk}px 0 grey`;
+};
+
+hero.addEventListener('mousemove', shadow);
+hero.addEventListener('mouseout', function () {
+  text.style.textShadow = `none`;
+});
+
 const hours = new Date().getHours();
 const min = new Date().getMinutes();
 const contentH = hours > 12 ? `at ${hours} pm` : `at ${hours} am`;
@@ -9,12 +33,12 @@ const contentM = ` and ${min} minutes`;
 document.querySelector('#minutes').innerHTML = contentM;
 document.querySelector('#year').innerHTML = contentH;
 
-function getCode () {
+const getCode = function () {
   const youtubecode = document.querySelector('.input-link').value;
   new LinkHandler(youtubecode).build();
-}
+};
 
-document.getElementById('Btn').onclick = getCode();
+document.getElementById('Btn').onclick = getCode;
 
 // functions
 function loadProfiles (userNames) {
@@ -22,7 +46,7 @@ function loadProfiles (userNames) {
     const output = 'Fetched for ' + userNames[i];
     console.log(output);
   }
-}
+};
 
 loadProfiles(['porto', 'sporting', 'benfica']);
 // es6 string methods
